dccc45cc4315ea1d09f2cf6b27cd1c995522b438
kidney <kidneyleung@gmail.com>
2017-03-10 16:54:15 +0800

    Feat: 实现调用gulp-iconfont将SVG生成字体

--- src/index.js
+++ src/index.js
@@ -5,11 +5,48 @@ const merge = require('merge');
 const gulp = require('gulp');
 const gulpIconfont = require('gulp-iconfont');
 
-function extractsSVG (css) {
+function errorLog(msg) {
+    console.error(msg);
+}
+
+function trimQuotationMarks(val) {
+    return val.replace(/^['"]{0,1}/, '').replace(/['"]{0,1}$/, '');
+}
+function include(arr,obj) {
+    return (arr.indexOf(obj) !== -1);
+}
+function noop () {}
+
+function createFontSrcDeclarationsWithEOT (options, result) {
+    let fontPath = path.join(options.publishPath, result.opts.fileName);
+    return 'url(\'' + fontPath + '.eot\')';
+}
+
+function createFontSrcDeclarations(options, result) {
+    let fontPath = path.join(options.publishPath, result.opts.fileName);
+    let formats = result.opts.formats;
+
+    let declarationArr = [];
+    if (include(formats, 'eot')) {
+        declarationArr.push('url(\'' + fontPath + '.eot?#iefix\') format(\'embedded-opentype\')');
+    }
+    if (include(formats, 'woff2')) {
+        declarationArr.push('url(\'' + fontPath + '.woff2\') format(\'woff2\')');
+    }
+    if (include(formats, 'woff')) {
+        declarationArr.push('url(\'' + fontPath + '.woff\') format(\'woff\')');
+    }
+    if (include(formats, 'svg')) {
+        declarationArr.push('url(\'' + fontPath + '.svg#' + result.opts.fontName + '\') format(\'svg\')');
+    }
+    return declarationArr.join(', ');
+}
+
+function extractsSVG (root) {
     let queueMap = [];
 
     // for each font face rule
-    css.walkAtRules('font-face', function (rule) {
+    root.walkAtRules('font-face', function (rule) {
         let srcPath;
         let fontName;
 
@@ -25,7 +62,7 @@ function extractsSVG (css) {
         // }
         // for each font-family declaration
         rule.walkDecls('font-family', function (decl) {
-            fontName = decl.value.replace(/^['"]{0,1}/, '').replace(/['"]{0,1}$/, '');
+            fontName = trimQuotationMarks(decl.value);
         });
 
         // if (!fontName) {
@@ -38,40 +75,23 @@ function extractsSVG (css) {
         });
     });
 
-    // queue.forEach(function (item) {
-    //     options.fontName = item.fontName;
-    //     generateFont({
-    //         srcPath: path.resolve(BASE_PATH, item.srcPath),
-    //         outputPath: OUTPUT_PATH,
-    //         iconFontOptions: options,
-    //         onComplete: function(glyphs, options) {
-    //             // CSS templating, e.g.
-    //             console.log(glyphs, options);
-    //         }
-    //     });
-    // });
-
-    return Promise.resolve([queueMap]);
+    return queueMap;
 }
 
-function prepare (options, queueMap) {
-    let outputPath = path.resolve(options.basePath, options.outputPath);
+function prepare (options, queueItem) {
+    let iconFontOptions = merge({
+        fontName: queueItem.fontName,
+        log: noop // disabled print log from svgfont2svgicons
+    }, options);
+    delete iconFontOptions.basePath;
+    delete iconFontOptions.outputPath;
+    delete iconFontOptions.publishPath;
 
-    queueMap.forEach(function (item, index) {
-        let iconFontOptions = merge({
-            fontName: item.fontName
-        }, options);
-        delete iconFontOptions.basePath;
-        delete iconFontOptions.outputPath;
-
-        queueMap[index] = {
-            srcPath: path.resolve(options.basePath, item.srcPath),
-            outputPath: outputPath,
-            iconFontOptions: iconFontOptions
-        };
-    });
-
-    return Promise.resolve([queueMap]);
+    return {
+        srcPath: path.resolve(options.basePath, queueItem.srcPath),
+        outputPath: path.resolve(options.basePath, options.outputPath),
+        iconFontOptions: iconFontOptions
+    };
 }
 
 function runGulpIconFont (queueItem) {
@@ -79,15 +99,91 @@ function runGulpIconFont (queueItem) {
         gulp.src([queueItem.srcPath])
             .pipe(gulpIconfont(queueItem.iconFontOptions))
             .on('glyphs', function (glyphs, opts) {
-                resolve([glyphs, opts]);
+                resolve({glyphs: glyphs, opts: opts});
             }).on('error', function () {
                 reject();
             }).pipe(gulp.dest(queueItem.outputPath));
     });
 }
 
-function writeStyleRules(glyphs, opts) {
+function insertFontSrcDeclarations(rule, options, result) {
+    rule.walkDecls('src', function (decl) {
+        // eot for < ie9
+        if (include(result.opts.formats, 'eot')) {
+            rule.insertBefore(decl, postcss.decl({
+                prop: 'src',
+                value: createFontSrcDeclarationsWithEOT(options, result)
+            }));
+        }
 
+        rule.insertBefore(decl, postcss.decl({
+            prop: 'src',
+            value: createFontSrcDeclarations(options, result)
+        }));
+
+        decl.remove();
+    });
+}
+
+function insertGlyphsRules(rule, options, result) {
+    result.glyphs.forEach(function (glyph) {
+        let node = postcss.rule({
+            selector: '.' + result.opts.fontName + '-' + glyph.name + ':before'
+        });
+        node.append(postcss.decl({
+            prop: 'content',
+            value: '\'\\' +
+                glyph.unicode[0]
+                    .charCodeAt(0)
+                    .toString(16)
+                    .toUpperCase()
+                + '\''
+        }));
+
+        rule.parent.insertAfter(rule, node);
+    });
+
+    let node = postcss.rule({
+        selector: '[class^="' + result.opts.fontName + '-"], [class*=" ' + result.opts.fontName + '-"]'
+    });
+    [
+        {prop: 'font-family', value: '\'' + result.opts.fontName + '\''},
+        {prop: 'speak', value: 'none'},
+        {prop: 'font-style', value: 'normal'},
+        {prop: 'font-weight', value: 'normal'},
+        {prop: 'font-variant', value: 'normal'},
+        {prop: 'text-transform', value: 'none'},
+        {prop: 'line-height', value: 1},
+        {prop: '-webkit-font-smoothing', value: 'antialiased'},
+        {prop: '-moz-osx-font-smoothing', value: 'grayscale'}
+    ].forEach(function (item) {
+        node.append(postcss.decl({
+            prop: item.prop,
+            value: item.value
+        }));
+    });
+
+    rule.parent.insertAfter(rule, node);
+}
+
+function updateRule(root, options, result) {
+    root.walkAtRules('font-face', function (rule) {
+        let isInsert = false;
+        rule.walkDecls('font-family', function (decl) {
+            if (trimQuotationMarks(decl.value) === result.opts.fontName) {
+                isInsert = true;
+            }
+        });
+
+        if (isInsert) {
+            insertFontSrcDeclarations(rule, options, result);
+            insertGlyphsRules(rule, options, result);
+
+            return false;
+        }
+    });
+
+    return root;
 }
 
 function iconFontPlugin (options) {
@@ -98,27 +194,20 @@ function iconFontPlugin (options) {
         formats: ['svg', 'ttf', 'eot', 'woff']
     }, (options || {}));
 
-    /*const BASE_PATH = options.basePath;
-    const OUTPUT_PATH = path.resolve(BASE_PATH, options.outputPath);
-    delete options.basePath;
-    delete options.outputPath;*/
+    return function (root, result) {
+        return Promise.map(extractsSVG(root), function(queueItem) {
+            return prepare(options, queueItem);
+        }).map(function (queueItem) { // run gulp-iconfont generate fonts.
+            return runGulpIconFont(queueItem);
+        }).map(function (result) { // update css rule.
+            return updateRule(root, options, result);
+        }).spread(function (root) {
 
-    return function (css, result) {
-        return extractsSVG(css)
-            .spread(function (queueMap) {
-                return prepare(options, queueMap);
-            })
-            .spread(function (queueMap) {
-                return runGulpIconFont(queueMap[0]);
-            })
-            .spread(function (glyphs, opts) {
-                return writeStyleRules(glyphs, opts);
-            })
-            .catch(function (err) {
-                console.error('postcss-iconfont: An error occurred while processing files - ' + err.message);
-                console.error(err.stack);
-                throw err;
-            });
+        }).catch(function (err) {
+            errorLog('postcss-iconfont: An error occurred while processing files - ' + err.message);
+            errorLog(err.stack);
+            throw err;
+        });
     };
 }
 
